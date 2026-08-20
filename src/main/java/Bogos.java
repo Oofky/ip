a38163e5eeb1597041d4d3b31d5ab87f6e25ecf6
keying <beensprouting@gmail.com>
2026-08-21 03:43:02 +0800

    Complete Level-1
    Echo

--- src/main/java/Bogos.java
+++ src/main/java/Bogos.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
+
 public class Bogos {
     public static void main(String[] args) {
-        String banner = """
+        String horizString = "____________________________________________________________";
+        String indentString = "         ";
+        String bannerString = """
       ___             __ _                  
      | _ )    ___    / _` |   ___     ___   
      | _ \\   / _ \\   \\__, |  / _ \\   (_-<   
@@ -9,10 +13,23 @@ public class Bogos {
    "`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'
 ____________________________________________________________
 Hey. I'm Bogos.
-Can I help you get your photos printed?
-____________________________________________________________
-Bogos binted.
-   """;
-        System.out.println(banner);
+Can I help you get your photos printed? (How can I help you?)""";
+        String byeString = "Bogos binted. (Bye!)";
+        System.out.println(bannerString);
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String command = scanner.nextLine();
+            System.out.println(horizString);
+
+            if (command.equals("bye")) {
+                System.out.println(indentString + byeString);
+                System.out.println(horizString);
+                break;
+            }
+
+            System.out.println(indentString + command);
+            System.out.println(horizString);
+        }
     }
 }
