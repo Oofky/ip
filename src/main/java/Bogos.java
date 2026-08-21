d8b1f2efaab78de96865593387c265850df781bf
keying <beensprouting@gmail.com>
2026-08-21 11:38:33 +0800

    Complete Level-2

--- src/main/java/Bogos.java
+++ src/main/java/Bogos.java
@@ -1,10 +1,9 @@
 import java.util.Scanner;
 
 public class Bogos {
-    public static void main(String[] args) {
-        String horizString = "____________________________________________________________";
-        String indentString = "         ";
-        String bannerString = """
+    private static final String HORIZ_STRING = "____________________________________________________________";
+    private static final String INDENT_STRING = "         ";
+    private static final String BANNER_STRING = """
       ___             __ _                  
      | _ )    ___    / _` |   ___     ___   
      | _ \\   / _ \\   \\__, |  / _ \\   (_-<   
@@ -14,22 +13,35 @@ public class Bogos {
 ____________________________________________________________
 Hey. I'm Bogos.
 Can I help you get your photos printed? (How can I help you?)""";
-        String byeString = "Bogos binted. (Bye!)";
-        System.out.println(bannerString);
+    private static final String BYE_STRING = "Bogos binted. (Bye!)";
+    private static final int MAX_TASKS = 100;
+    private static final String[] tasks = new String[MAX_TASKS];
+
+    public static void main(String[] args) {
+        System.out.println(BANNER_STRING);
 
         Scanner scanner = new Scanner(System.in);
+        int numberOfTasks = 0;
+
         while (true) {
             String command = scanner.nextLine();
-            System.out.println(horizString);
+            System.out.println(HORIZ_STRING);
 
             if (command.equals("bye")) {
-                System.out.println(indentString + byeString);
-                System.out.println(horizString);
+                System.out.println(INDENT_STRING + BYE_STRING);
+                System.out.println(HORIZ_STRING);
                 break;
+            } else if (command.equals("list")) {
+                for (int i = 0; i < numberOfTasks; i++) {
+                    System.out.println(INDENT_STRING + (i + 1) + ". " + tasks[i]);
+                }
+            } else if (numberOfTasks < MAX_TASKS) { // Defensive check
+                tasks[numberOfTasks] = command;
+                numberOfTasks++;
+                System.out.println(INDENT_STRING + "added: " + command);
             }
 
-            System.out.println(indentString + command);
-            System.out.println(horizString);
+            System.out.println(HORIZ_STRING);
         }
     }
 }
