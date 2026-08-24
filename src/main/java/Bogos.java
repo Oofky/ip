04d94ebe0f47337168ba556c5691ef3b9c066d29
keying <beensprouting@gmail.com>
2026-08-24 11:18:22 +0800

    Implement task completion

--- src/main/java/Bogos.java
+++ src/main/java/Bogos.java
@@ -11,11 +11,12 @@ public class Bogos {
    _|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"| 
    "`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'
 ____________________________________________________________
-Hey. I'm Bogos.
-Can I help you get your photos printed? (How can I help you?)""";
-    private static final String BYE_STRING = "Bogos binted. (Bye!)";
+Blessings! Bogos beckons. Bring Bogos business? :]""";
+    private static final String BYE_STRING = "Bye bye! :]";
     private static final int MAX_TASKS = 100;
     private static final String[] tasks = new String[MAX_TASKS];
+    // Records whether the task at the corresponding index in tasks is complete
+    private static final boolean[] isDone = new boolean[MAX_TASKS];
 
     public static void main(String[] args) {
         System.out.println(BANNER_STRING);
@@ -32,13 +33,30 @@ Can I help you get your photos printed? (How can I help you?)""";
                 System.out.println(HORIZ_STRING);
                 break;
             } else if (command.equals("list")) {
+                System.out.println(INDENT_STRING + "Behold bullets:");
                 for (int i = 0; i < numberOfTasks; i++) {
-                    System.out.println(INDENT_STRING + (i + 1) + ". " + tasks[i]);
+                    System.out.println(INDENT_STRING + (i + 1) + ".["
+                            + (isDone[i] ? "X" : " ") + "] " + tasks[i]);
+                }
+            } else if (command.startsWith("mark ")) {
+                String taskNumberText = command.substring("mark ".length()).trim();
+                try {
+                    int taskNumber = Integer.parseInt(taskNumberText);
+                    if (taskNumber < 1 || taskNumber > numberOfTasks) {
+                        System.out.println(INDENT_STRING + "Bummer. Bullet beyond bounds. :[");
+                    } else {
+                        int taskIndex = taskNumber - 1;
+                        isDone[taskIndex] = true;
+                        System.out.println(INDENT_STRING + "Bravo! Bogos boxed bullet:");
+                        System.out.println(INDENT_STRING + "  [X] " + tasks[taskIndex]);
+                    }
+                } catch (NumberFormatException exception) {
+                    System.out.println(INDENT_STRING + "Bogus. Bring Bogos base-ten. :[");
                 }
             } else if (numberOfTasks < MAX_TASKS) { // Defensive check
                 tasks[numberOfTasks] = command;
                 numberOfTasks++;
-                System.out.println(INDENT_STRING + "added: " + command);
+                System.out.println(INDENT_STRING + "bullet born: " + command);
             }
 
             System.out.println(HORIZ_STRING);
