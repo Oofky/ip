c5ae5c2719b42d967ce2ca6c0cf69cbdd2ba76e4
keying <beensprouting@gmail.com>
2026-08-26 17:00:00 +0800

    Complete Level-5 + Fix code formatting
    Handle Errors
    + Made repeated code into helper funtions
    - Haven't run Codex's test-ui skill

--- src/main/java/Bogos.java
+++ src/main/java/Bogos.java
@@ -15,95 +15,110 @@ Blessings! Bogos beckons. Bring Bogos business? :]""";
     private static final String BYE_STRING = "Bye bye! :]";
     private static final int MAX_TASKS = 100;
     private static final Task[] tasks = new Task[MAX_TASKS];
+    private static int numberOfTasks = 0;
 
     public static void main(String[] args) {
         System.out.println(BANNER_STRING);
-
         Scanner scanner = new Scanner(System.in);
-        int numberOfTasks = 0;
 
         while (true) {
             String command = scanner.nextLine();
             System.out.println(HORIZ_STRING);
 
             if (command.equals("bye")) {
-                System.out.println(INDENT_STRING + BYE_STRING);
+                bogosSay(BYE_STRING);
                 System.out.println(HORIZ_STRING);
                 break;
-            } else if (command.equals("list")) {
-                System.out.println(INDENT_STRING + "Behold bullets:");
-                for (int i = 0; i < numberOfTasks; i++) {
-                    System.out.println(INDENT_STRING + (i + 1) + "." + tasks[i].toString());
-                }
-            } else if (command.startsWith("mark ") || command.startsWith("unmark ")) {
-                boolean mark = command.startsWith("mark");
-                String taskNumberText = command.substring(mark ? "mark ".length() : "unmark ".length()).trim();
-                try {
-                    int taskNumber = Integer.parseInt(taskNumberText);
-                    if (taskNumber < 1 || taskNumber > numberOfTasks) {
-                        System.out.println(INDENT_STRING + "Bummer. Bullet beyond bounds. :[");
-                    } else {
-                        int taskIndex = taskNumber - 1;
-                        Task task = tasks[taskIndex];
-
-                        if (task.isDone() == mark) { // Redundant action
-                            System.out.println(INDENT_STRING + "Bro, box basically behaved beforehand.");
-                        } else if (mark) {
-                            task.markAsDone();
-                            System.out.println(INDENT_STRING + "Bravo! Bogos boxed bullet:");
-                            System.out.println(INDENT_STRING + "  " + task.toString());
-                        } else {
-                            task.markAsNotDone();
-                            System.out.println(INDENT_STRING + "Bet! Bogos blanked box:");
-                            System.out.println(INDENT_STRING + "  " + task.toString());
+            } 
+            
+            try {
+                if (command.equals("list")) {
+                    if (numberOfTasks > 0) {
+                        bogosSay("Behold bulleted board:");
+                        for (int i = 0; i < numberOfTasks; i++) {
+                            bogosSay((i + 1) + "." + tasks[i].toString());
                         }
-                    }
-                } catch (NumberFormatException exception) {
-                    System.out.println(INDENT_STRING + "Bogus. Bring Bogos base-ten. :[");
-                }
-            } else if (command.startsWith("todo ")) {
-                String todoText = command.substring("todo ".length()).trim();
-                Task newTask = new Todo(todoText);
-                tasks[numberOfTasks] = newTask;
-                numberOfTasks++;
-                System.out.println(INDENT_STRING + "Boom! Bullet born: ");
-                System.out.println(INDENT_STRING + "  " + newTask.toString());
-                System.out.println(INDENT_STRING + numberOfTasks + " bullet(s) being.");
-            } else if (command.startsWith("deadline ")) {
-                int byIndex = command.indexOf(" /by ");
-                if (byIndex > "deadline".length()) { // Check if /by exists AND if there's a task description
-                    String deadlineText = command.substring("deadline ".length(), byIndex).trim();
-                    String by = command.substring(byIndex + " /by ".length()).trim();
-                    Task newTask = new Deadline(deadlineText, by);
-                    tasks[numberOfTasks] = newTask;
-                    numberOfTasks++;
-                    System.out.println(INDENT_STRING + "Boom! Bullet born: ");
-                    System.out.println(INDENT_STRING + "  " + newTask.toString());
-                    System.out.println(INDENT_STRING + numberOfTasks + " bullet(s) being.");
-                } else {
-                    System.out.println(INDENT_STRING + "bwhat");
-                }
-            } else if (command.startsWith("event ")) {
-                int fromIndex = command.indexOf(" /from ");
-                int toIndex = command.indexOf(" /to ");
-                if (fromIndex > "event".length() && toIndex > fromIndex) {
-                    String eventText = command.substring("event ".length(), fromIndex).trim();
-                    String starting = command.substring(fromIndex + " /from ".length(), toIndex).trim();
-                    String ending = command.substring(toIndex + " /to ".length()).trim();
-                    Task newTask = new Event(eventText, starting, ending);
-                    tasks[numberOfTasks] = newTask;
-                    numberOfTasks++;
-                    System.out.println(INDENT_STRING + "Boom! Bullet born: ");
-                    System.out.println(INDENT_STRING + "  " + newTask.toString());
-                    System.out.println(INDENT_STRING + numberOfTasks + " bullet(s) being.");
-                } else {
-                    System.out.println(INDENT_STRING + "bwhat");
-                }
-            } else {
-                System.out.println(INDENT_STRING + "bwhat");
-            }
+                    } else { throw new BogosException("But board be blank..."); }
+                    
+                } else if (command.startsWith("mark ") || command.startsWith("unmark ")) {
+                    boolean mark = command.startsWith("mark");
+                    String taskNumberText = command.substring(mark ? "mark ".length() : "unmark ".length()).trim();
+                    try {
+                        int taskNumber = Integer.parseInt(taskNumberText);
+                        if (taskNumber < 1 || taskNumber > numberOfTasks) {
+                            throw new BogosException("Bummer. Bullet beyond bounds. :[");
+                        } else {
+                            int taskIndex = taskNumber - 1;
+                            Task task = tasks[taskIndex];
 
-            System.out.println(HORIZ_STRING);
+                            if (task.isDone() == mark) { // Redundant action
+                                throw new BogosException("Bro, box basically behaved beforehand.");
+                            } else if (mark) {
+                                task.markAsDone();
+                                bogosSay("Bravo! Bogos boxed bullet:");
+                                bogosSay("  " + task.toString());
+                            } else {
+                                task.markAsNotDone();
+                                bogosSay("Bet! Bogos blanked box:");
+                                bogosSay("  " + task.toString());
+                            }
+                        }
+                    } catch (NumberFormatException exception) {
+                        throw new BogosException("Bogus. Bring Bogos base-ten. :[");
+                    }
+
+                } else if (command.startsWith("todo ")) {
+                    String todoText = command.substring("todo ".length()).trim();
+                    verifyInputBlank(todoText);
+                    addTask(new Todo(todoText));
+                    
+                } else if (command.startsWith("deadline ")) {
+                    int byIndex = command.indexOf(" /by ");
+                    if (byIndex >= "deadline ".length()) { // Check if /by exists and is not empty string
+                        String deadlineText = command.substring("deadline ".length(), byIndex).trim();
+                        String by = command.substring(byIndex + " /by ".length()).trim();
+                        verifyInputBlank(deadlineText, by);
+                        addTask(new Deadline(deadlineText, by));
+                    } else { throw new BogosException("bwhat [deadline ... /by ...]"); }
+
+                } else if (command.startsWith("event ")) {
+                    int fromIndex = command.indexOf(" /from ");
+                    int toIndex = command.indexOf(" /to ");
+                    if (fromIndex >= "event ".length() && toIndex >= fromIndex) { // Check if /from and /to exists and are not empty strings
+                        String eventText = command.substring("event ".length(), fromIndex).trim();
+                        String starting = command.substring(fromIndex + " /from ".length(), toIndex).trim();
+                        String ending = command.substring(toIndex + " /to ".length()).trim();
+                        verifyInputBlank(eventText, starting, ending);
+                        addTask(new Event(eventText, starting, ending));
+                    } else { throw new BogosException("bwhat [event ... /from ... /to ...]"); }
+
+                } else {
+                    throw new BogosException("bwhat");
+                }
+
+            } catch (BogosException e) {
+                bogosSay(e.getMessage());
+            } finally {
+                System.out.println(HORIZ_STRING);
+            }
+        }
+    }
+
+    private static void bogosSay(String message) {
+        System.out.println(INDENT_STRING + message);
+    }
+
+    private static void addTask(Task newTask) {
+        tasks[numberOfTasks] = newTask;
+        numberOfTasks++;
+        bogosSay("Boom! Bullet born: ");
+        bogosSay("  " + newTask.toString());
+        bogosSay(Integer.toString(numberOfTasks) + " bullet(s) being.");
+    }
+
+    private static void verifyInputBlank(String... inputs) throws BogosException {
+        for (String s : inputs) {
+            if (s.isBlank()) { throw new BogosException("bwhat body"); }
         }
     }
 }
---
+++ src/main/java/BogosException.java
@@ -0,0 +1,5 @@
+public class BogosException extends Exception {
+    public BogosException(String message) {
+        super(message);
+    }
+}
