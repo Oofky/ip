81a16ae63b625c11332194dd07e86d327157bfc0
keying <beensprouting@gmail.com>
2026-09-09 13:24:15 +0800

    Add basic tests for parseTask

--- src/test/java/bogos/ParserTest.java
+++ src/test/java/bogos/ParserTest.java
@@ -3,15 +3,32 @@ package bogos;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class ParserTest {
     @Test
-    public void dummyTest(){
-        assertEquals(2, 2);
+    public void parseTask_todo_success() throws BogosException {
+        Parser parser = new Parser();
+
+        Task task = parser.parseTask("todo CS3241 assignment");
+
+        Todo todo = assertInstanceOf(Todo.class, task);
+        assertEquals("CS3241 assignment", todo.getDescription());
+        assertFalse(todo.isDone());
     }
 
     @Test
-    public void anotherDummyTest(){
-        assertEquals(4, 4);
+    public void parseTask_nonsense_exceptionThrown() {
+        Parser parser = new Parser();
+
+        try {
+            Task task = parser.parseTask("todooles homework");
+            fail(); // the test should not reach this line
+        } catch (BogosException e) {
+            assertEquals("bwhat", e.getMessage());
+        }
     }
+
 }
